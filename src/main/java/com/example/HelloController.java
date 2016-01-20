33cefeb86f719d8499418bf6a9b03ef14d55aa61
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2016-01-20 17:37:28 +0900

    Add HelloController

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Created by Hirohisa Kawasaki on 2016/01/20.
+ */
+
+import org.springframework.web.bind.annotation.*;
+
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String hello() {
+        return "Hello Spring MVC";
+    }
+}
